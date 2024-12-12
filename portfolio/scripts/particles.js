e3a7ac970985e25e6871eff9cbb5c41300786b71
Vijay Reddy <vijayreddy6812@gmail.com>
2024-12-12 17:19:04 -0500

    Create particles.js

---
+++ portfolio/scripts/particles.js
@@ -0,0 +1,11 @@
+particlesJS("particles-js", {
+    particles: {
+        number: { value: 100, density: { enable: true, value_area: 800 } },
+        color: { value: "#ffffff" },
+        shape: { type: "circle" },
+        opacity: { value: 0.5 },
+        size: { value: 3 },
+        line_linked: { enable: true, color: "#ffffff", width: 1 },
+        move: { enable: true, speed: 2 }
+    }
+});
